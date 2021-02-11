fe2ec91d5c94a5484bc2bcb46df416b4cd406d67
Tanuj shrivastava <63875409+Tanuj-tj@users.noreply.github.com>
2021-02-11 23:29:52 +0530

    Update String_Palindrome.cpp

--- Recursion/String_Palindrome.cpp
+++ Recursion/String_Palindrome.cpp
@@ -27,6 +27,20 @@ void powerset(string s,int i,string cur){
     powerset(s,i+1,cur);
 }
 
+// 3.
+
+void permute(string s,string ans){
+    if(s.length()==0){
+        cout<<ans<<endl;
+        return;
+    }
+    for(int i=0;i<s.length();i++){
+        char ch = s[i];
+        string ros = s.substr(0,i) + s.substr(i+1);
+        permute(ros,ans+ch);
+    }
+}
+
 int main(){
     
     string s = "racecar";
@@ -35,5 +49,7 @@ int main(){
     string s1 = "abc";
     powerset(s1,0,"");
   
+    string s2 = "abc";
+    permute(s2,"");
     return 0; 
 }
