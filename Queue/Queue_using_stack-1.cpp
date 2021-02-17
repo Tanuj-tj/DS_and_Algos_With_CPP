79a099c3d168736a52cec3347839617361e74115
Tanuj shrivastava <63875409+Tanuj-tj@users.noreply.github.com>
2021-02-17 20:12:13 +0530

    Add files via upload

---
+++ Queue/Queue_using_stack-1.cpp
@@ -0,0 +1,61 @@
+// Approach 1: Using 2 stack and deQueue operation costly  
+
+#include<iostream>
+#include<stack>
+using namespace std;
+
+class Queue{
+    stack<int> s1;
+    stack<int> s2;
+
+    public:
+
+    void push(int x){
+        s1.push(x);        // O(1)
+    }
+
+    int pop(){
+        while(s1.empty() && s2.empty()){
+            cout<<"Queue is empty"<<endl;
+            return -1;
+        }
+        if(s2.empty()){
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        int topvalue = s2.top();
+        s2.pop();
+        return topvalue;
+    }
+
+    bool empty(){
+        if(s1.empty()&&s2.empty()){
+            return true;
+        }
+        return false;
+    
+    }
+};
+
+
+int main(){
+
+    Queue q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    cout<<q.pop()<<endl;
+    q.push(5);
+    cout<<q.empty()<<endl;
+    cout<<q.pop()<<endl;
+    cout<<q.pop()<<endl;
+    cout<<q.pop()<<endl;
+    cout<<q.pop()<<endl;
+    cout<<q.pop()<<endl;
+    cout<<q.pop()<<endl;
+
+    return 0;
+}
