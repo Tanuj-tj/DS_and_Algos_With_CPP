f46ae605d7ec5c58dbd994bdd3af3720c1df4ba1
Tanuj shrivastava <63875409+Tanuj-tj@users.noreply.github.com>
2021-03-22 18:24:13 +0530

    Update Bubble_Sort.cpp

--- Sorting/Bubble_Sort.cpp
+++ Sorting/Bubble_Sort.cpp
@@ -1,29 +1,31 @@
 #include<iostream>
 using namespace std;
 
-void swap(int arr[],int i,int j){
-    int temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
+void swap(int a[],int i,int j){
+    int temp = a[i];
+    a[i] = a[j];
+    a[j] = temp;
 }
 
-int Bubble_sort(int arr[],int n){
-    bool swapped = true;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n-i-1;j++){
-            swapped = false;
-            swap(arr,j+1,j)
-        }
-        if(!swapped){
-            break;
-        }
-    }
-}
 int main(){
 
     int arr[] = {4,3,7,1,5};
     int n = 5;
-    cout<<Bubble_sort(arr,n)<<endl;
+    
+    bool swapped = true;
+    for(int i=0;i<n;i++){                      
+        for(int j=0;j<n-i-1;j++){
+           swapped = false;
+           if(arr[j+1]<arr[j])
+                swap(arr,j+1,j);
+        }
+        if(swapped)
+            break;
+    }
+    
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
 
     return 0;
 }
