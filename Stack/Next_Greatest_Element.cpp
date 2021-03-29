038e332ab78d166491ade95b8ff6635c28d7075f
Tanuj shrivastava <63875409+Tanuj-tj@users.noreply.github.com>
2021-03-29 12:49:52 +0530

    Create Next_Greatest_Element.cpp

---
+++ Stack/Next_Greatest_Element.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> next_greatest(vector<int> v1){
+    vector<int> v;       
+    stack<int> s;
+
+    for(int i=v1.size()-1;i>=0;i--){
+        if(s.size()==0){
+            v.push_back(-1);
+        }
+        else if(s.size() > 0 && s.top() > v1[i]){         
+            v.push_back(s.top());
+        }
+        else if(s.size() > 0 && s.top() <= v1[i]){       // 3 <= 3
+            while(s.size() > 0 && s.top() <= v1[i]){     // 
+                s.pop();
+            }
+            if(s.size()==0){
+                v.push_back(-1);
+            }
+            else{
+                v.push_back(s.top());
+            }
+        }
+        s.push(v1[i]);
+    }
+
+    reverse(v.begin(),v.end());
+
+    return v;
+
+}
+
+int main(){
+    vector<int> v1 = {11,13,21,3}; // [-1] = False
+  
+    cout<<"Input Array: ";
+    for(int i=0;i<v1.size();i++){
+        cout<<v1[i]<<" ";
+    }
+
+    cout<<"Output Array: ";
+    for(int i=0;i<v1.size();i++){
+        cout<<next_greatest(v1)[i]<<" ";
+    }
+    
+
+    return 0;
+}
