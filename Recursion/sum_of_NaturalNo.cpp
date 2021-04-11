c1086d5ecc450646ddd4e6c64712b3fde0561318
Tanuj shrivastava <63875409+Tanuj-tj@users.noreply.github.com>
2021-04-11 19:17:00 +0530

    Update sum_of_NaturalNo.cpp

--- Recursion/sum_of_NaturalNo.cpp
+++ Recursion/sum_of_NaturalNo.cpp
@@ -1,15 +1,30 @@
 #include<iostream>
 using namespace std;
 
-int sum(int n){
+// Time : O(1) Space : O(1)
+int Formula(int n){
+    return n * (n + 1) / 2;
+}
+// Time : O(n) Space : O(1)
+int Loop(int n){
+    int i,s=0;
+    for(int i=0;i<=n;i++){
+        s=s+i;
+    }
+    return s;
+}
+// Time : O(n) Space : O(n)
+int Recursion(int n){
     if(n==1)
         return 1;
 
-    return n + sum(n-1);
+    return n + Recursion(n-1);
 }
 
 int main(){
 
-    int n = 3;
-    cout<<sum(n)<<endl;
+    int n = 5;
+    cout<<"Using Direct Formula : "<<Formula(n)<<endl;
+    cout<<"Using Loop : "<<Loop(n)<<endl;
+    cout<<"Using Recursion : "<<Recursion(n)<<endl;
 }
