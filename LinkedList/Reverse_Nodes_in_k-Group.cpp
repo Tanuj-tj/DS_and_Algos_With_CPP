1a2022994e0d2c49cc7749ac476ec83c08cbb3a8
Tanuj shrivastava <63875409+Tanuj-tj@users.noreply.github.com>
2021-05-04 21:58:51 +0530

    Update Reverse_Nodes_in_k-Group.cpp

--- LinkedList/Reverse_Nodes_in_k-Group.cpp
+++ LinkedList/Reverse_Nodes_in_k-Group.cpp
@@ -8,33 +8,74 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* reverseKGroup(ListNode* head, int k) {
+        
+//         ListNode* next;      // next = head->next;
+//         ListNode* prev = NULL;
+//         ListNode* curr = head;
+        
+//         int count = 0;
+      
+//         // Reverse first k nodes
+//         while(curr!=NULL && count<k){
+//             next = curr->next;
+//             curr->next = prev;
+            
+//             prev = curr;
+//             curr = next;
+        
+//             count++;
+//         }
+        
+//         // Recursive Call
+//         if(next!=NULL){
+//             head->next = reverseKGroup(next,k);
+//         }
+        
+//         return prev;    
+//     }
+    
+// };
+
+
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
         
-        ListNode* next;      // next = head->next;
-        ListNode* prev = NULL;
-        ListNode* curr = head;
+        if(head == NULL || k==1) return head;
+        
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        
+        ListNode* nextptr = dummy;      
+        ListNode* prev = dummy;
+        ListNode* curr = dummy;
         
         int count = 0;
-      
-        // Reverse first k nodes
-        while(curr!=NULL && count<k){
-            next = curr->next;
-            curr->next = prev;
-            
-            prev = curr;
-            curr = next;
-        
-            count++;
+        while(curr->next!=NULL){
+            curr=curr->next;
+            count++;  
         }
         
-        // Recursive Call
-        if(next!=NULL){
-            head->next = reverseKGroup(next,k);
-        }
+        curr = head;
         
-        return prev;    
+        
+         while(count>=k){
+             curr = prev->next;
+             nextptr = curr->next;
+             for(int i=1;i<k;i++){
+                 curr->next = nextptr->next;
+                 nextptr->next = prev->next;
+                 prev->next = nextptr;
+                 nextptr = curr->next;
+             }
+             prev = curr;
+             count-=k;
+         } 
+        
+        return dummy->next;
     }
     
 };
