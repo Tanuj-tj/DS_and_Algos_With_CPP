1217642615c435e3ea55eea5a6c8a25a8d8d90d7
Tanuj shrivastava <63875409+Tanuj-tj@users.noreply.github.com>
2021-05-20 21:55:44 +0530

    Add files via upload

---
+++ Coding_Minutes/Arrays_&_Vectors/Min_Swaps.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countMinSwaps(vector<int> &arr){
+
+    int n = arr.size(); 
+
+    // Keep track of indices of current array
+    pair<int,int> p[n];
+    for(int i=0;i<n;i++){
+        p[i].first = arr[i];
+        p[i].second = i;
+    }
+    // Sort the pair 
+    sort(p,p+n); 
+
+    // Keep Track of visited elements
+    vector<int> visited(n,false);
+    int ans=0;
+    for(int i=0;i<n;i++){
+
+        // If the element is at its right postion or it is already visited
+        int old_position = p[i].second;
+        if(visited[i]==true or old_position == i){
+            continue;
+        }
+
+        // Visiting the element for the first time
+        int node = i;
+        int cycle = 0;
+
+        while(visited[node]==false){
+            visited[node] = true;
+            int next_node = p[node].second;
+            node = next_node;
+            cycle += 1;
+        }
+        ans += (cycle-1);
+    }
+    return ans;    
+
+}
+
+int main(){
+    vector<int> arr = {5,4,3,2,1};
+    cout<<countMinSwaps(arr)<<endl;
+    return 0;
+}
