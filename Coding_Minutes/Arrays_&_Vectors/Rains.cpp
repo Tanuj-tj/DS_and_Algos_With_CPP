e1f245b8a7106362d484cfcf4345608d4cbe30db
Tanuj shrivastava <63875409+Tanuj-tj@users.noreply.github.com>
2021-05-29 10:53:16 +0530

    Update Rains.cpp

--- Coding_Minutes/Arrays_&_Vectors/Rains.cpp
+++ Coding_Minutes/Arrays_&_Vectors/Rains.cpp
@@ -1,3 +1,14 @@
+/*
+Given n non-negative integers representing an elevation
+map where the width of each bar is 1, compute how much
+water it can trap after raining.
+
+Sample Input :
+height =[0,1,0,2,1,0,1,3,2,1,2,1]
+
+Sample Output :
+6
+*/
 // O(N+N) = Time
 // O(N+N) => Space 
 
