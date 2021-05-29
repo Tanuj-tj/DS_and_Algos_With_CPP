641a6f581e77f2bc150b73b6eb81bd24f3494da0
Tanuj shrivastava <63875409+Tanuj-tj@users.noreply.github.com>
2021-05-29 10:46:55 +0530

    Update Array_Products.cpp

--- Coding_Minutes/Arrays_&_Vectors/Array_Products.cpp
+++ Coding_Minutes/Arrays_&_Vectors/Array_Products.cpp
@@ -1,3 +1,22 @@
+/*
+Implement a function that takes in a vector of integers, and returns a vector of the same length,
+where each element in the output array is equal to the product of every other number in the
+input array. Solve this problem without using division.
+
+In other words, the value at output[i] is equal to the product of every number in the input array
+other than input[i]. You can assume that answer can be stored inside int datatype and no-
+overflow will occur due to products.
+
+Sample Input :
+
+Both inputs and outputs are vectors.
+{1,2,3,4,5}
+
+Sample Output :
+{120, 6e, 4e, 3e, 24}
+
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
