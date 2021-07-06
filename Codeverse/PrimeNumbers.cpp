4affceaf6a3a9f8598c21a1bd1bc4270e294ef2e
Tanuj shrivastava <63875409+Tanuj-tj@users.noreply.github.com>
2021-07-06 22:06:11 +0530

    Create PrimeNumbers.cpp

---
+++ Codeverse/PrimeNumbers.cpp
@@ -0,0 +1,10 @@
+/*
+Facts about Prime Numbers 
+-------------------------
+
+1. Two is the only even prime
+2. Every prime can be written as 6b + 1 or 6n - 1, except 2 and 3, n is a natural number
+3. Goldbach Conjecture: Every even integer greater then 2 can be expressed as the sum of two primes
+4. Wilson's Theorem : (p-1)!=(p-1) mod p
+
+*/
