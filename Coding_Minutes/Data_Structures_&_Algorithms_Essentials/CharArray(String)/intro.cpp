e59d98ddf37f3e0adce22e9a5d72bddb7dc32032
Tanuj shrivastava <63875409+Tanuj-tj@users.noreply.github.com>
2021-09-05 10:40:59 +0530

    Update intro.cpp

--- Coding_Minutes/Data_Structures_&_Algorithms_Essentials/CharArray(String)/intro.cpp
+++ Coding_Minutes/Data_Structures_&_Algorithms_Essentials/CharArray(String)/intro.cpp
@@ -8,18 +8,28 @@ int main(){
 	cout<<b<<endl;
 	cout<<strlen(b)<<endl; // 3
 	cout<<sizeof(b)<<endl; // 4
-
+        
+	// Methods for taking User Input
+	
+	// 1.
 	char c[100];
 	cin>>c;    // If space is in between a i/p sting then it will consider it as a new line
 	cout<<c<<endl;
 
-
+        // 2.
 	char temp = cin.get();
 
 	while(temp != '?'){
 		cout<< temp;
 		temp = cin.get();
 	}
+	
+	// 3. 
+	char sentence[1000];
+
+	cin.getline(sentence,1000);
+
+	cout<<sentence<<endl;
    
 
 	return 0;
