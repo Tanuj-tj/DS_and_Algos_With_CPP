8728a5c9305a3bca713baf687912eea0f91d7fc8
Tanuj shrivastava <63875409+Tanuj-tj@users.noreply.github.com>
2021-09-06 20:45:22 +0530

    Add files via upload

---
+++ Coding_Minutes/Data_Structures_&_Algorithms_Essentials/2D_Array/Wave_Print.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define vii vector<vector<int>>
+
+vector<int> WavePrint(int m, int n, vector<vector<int>> arr)
+{
+    // your code goes here
+	int top = 0, down = m-1 , left = 0, right = n-1, temp = 1, check = n;
+    vi ans;
+  
+    while(check){
+    	if(temp == 1){
+    		for(int i = top ; i <= down ; i++){
+				ans.push_back(arr[i][right]);
+			}
+			temp = 0;
+			right--;
+			check--;
+    	}
+    	else{
+    		for(int i = down ; i >= top ; i--){
+				ans.push_back(arr[i][right]);
+			}
+			temp = 1;
+			right--;
+			check--;		
+    	}
+	
+
+	// for(int i = top ; i <= down ; i++){
+	// 	ans.push_back(arr[i][right]);
+	// }
+	// right--;
+
+	// for(int i = down ; i >= top ; i--){
+	// 	//cout<<i<<" ";
+	// 	ans.push_back(arr[i][right]);
+	// }
+	}
+	return ans;
+
+
+}
+
+int main(){
+
+	vii v{{1,2,3,4},
+		  {5,6,7,8},
+		  {9,10,11,12},
+  		  {13,14,15,16}};
+
+  	int m = v.size() ,n = v[0].size();
+
+	auto ans = WavePrint(m,n,v);
+	for(auto i : ans){
+		cout<<i<<" ";
+	}
+
+	return 0;
+}
